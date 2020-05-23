7150763e4d06b26d14d6af0348874573652dc9de
mallo106 <malloryluke14@gmail.com>
2020-05-23 10:58:50 -0500

    Recipe alert performance enhancements
    cache the websites when we've determined if it has a recipe or not so we don't end up rerunning the DOM parsing script all the time

--- src/js/background.js
+++ src/js/background.js
@@ -1,30 +1,42 @@
 chrome.tabs.onActivated.addListener(anActiveInfo => {
-  updateIcon(anActiveInfo.tabId)
+  updateIcon(anActiveInfo.tabId, true)
 });
 
 chrome.tabs.onUpdated.addListener((aTabId, anInfo) => {
-    updateIcon(aTabId);
+    updateIcon(aTabId, anInfo.status === "complete");
 });
+
+const kCachedAddresses = {};
 
 /**
  * update the extension icon based on if there's a recipe on the page or not
  * @param theTabId
  */
-function updateIcon(theTabId) {
+function updateIcon(theTabId, theFinalizeFindings) {
   chrome.tabs.get(theTabId,(aCurrent) => {
     if (!Boolean(aCurrent.url) || !aCurrent.url.startsWith('http')) {
       setIcon(false);
       return;
     }
+    const anAddress = aCurrent.url;
+    //Short circuit the call to microdata-to-json.js if we have this address cached
+    if (kCachedAddresses[anAddress] !== undefined || kCachedAddresses[anAddress]) {
+      setIcon(Boolean(kCachedAddresses[anAddress]));
+      return;
+    }
     chrome.tabs.executeScript(theTabId,
       {file: `js/microdata-to-json.js`}, (aResults) =>  {
-        setIcon(Boolean(aResults) && Boolean(aResults[0]))
+        const aHasRecipe = Boolean(aResults) && Boolean(aResults[0]);
+        if (aHasRecipe || theFinalizeFindings) {
+          kCachedAddresses[anAddress] = aHasRecipe;
+        }
+        setIcon(aHasRecipe);
       });
   });
 }
 
-function setIcon(theHasSuffix) {
-  const aSuffix = !theHasSuffix ? '' : '_alert';
+function setIcon(theHasRecipe) {
+  const aSuffix = !theHasRecipe ? '' : '_alert';
   chrome.browserAction.setIcon({
     path: {
       "18": `icons/baseline_fastfood_black_18dp${aSuffix}.png`,
